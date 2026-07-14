a0dd94f550815a2ee14779e9b00c73eee3b1b236
mediatuottajavantaa <jyrgenhuhtala@gmail.com>
2026-07-14 15:01:55 +0300

    3.7

---
+++ osa3/index.js
@@ -0,0 +1,89 @@
+let persons = require("./persons");
+
+const morgan = require("morgan");
+const express = require('express');
+const app = express();
+
+morgan.token('type', function (req, res) { return req.headers['content-type'] })
+app.use(morgan(":type"));
+app.use(express.json());
+
+app.get('/api/info', (req, res) => {
+    const infoHTML = generateInfoHTML();
+    res.send(infoHTML);
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons);
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = req.params.id;
+    const person = persons.find(person => person.id === id);
+
+    if (person) {
+        res.json(person);
+    } else {
+        res.status(404).end();
+    }
+})
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body;
+
+    if (!body.name) {
+        return res.status(400).json({error: 'name is required'});
+    }
+
+    if (!body.number) {
+        return res.status(400).json({error: 'number is required'});
+    }
+
+    const existingPerson = persons.find(person => person.name.toLowerCase() === body.name.toLowerCase())
+
+    if (existingPerson) {
+        return res.status(400).json({error: 'given name is already in PhoneBook'});
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: String(generateID())
+    }
+
+    persons = persons.concat(person);
+    res.json(person);
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = req.params.id;
+    persons = persons.filter(note => note.id !== id);
+
+    res.status(204).end();
+})
+
+const generateInfoHTML = () => {
+    const date = new Date();
+    return `
+    <p>Phonebook has info for ${persons.length} persons</p>
+    <p>${date}</p>
+    `
+}
+
+const generateID = () => {
+    function randomIntFromInterval(min, max) { // min and max included
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+    return randomIntFromInterval(1, 1000000);
+}
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
