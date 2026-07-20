71f92de3a840e4edb1d19e682aa1823e3b63465c
Jyri Huhtala <jyrgenhuhtala@gmail.com>
2026-07-20 13:58:25 +0300

    aloitettu 4.2

--- osa4/index.js
+++ osa4/index.js
@@ -1,43 +1,27 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
-const { info, error }= require('./utils/logger')
+const logger = require('./utils/logger')
+const blogsRouter = require('./controllers/blogs')
+const { requestLogger } = require('./utils/middleware')
 
 const app = express()
-
-const blogSchema = mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
+logger.info('Connecting to DB...')
+app.use(express.json())
 
 mongoose
     .connect(config.MONGODB_URI, { family: 4 })
     .then(() => {
-        info('connected to MongoDB')
+        logger.info('connected to MongoDB')
     })
     .catch((error) => {
-        error('error connection to MongoDB:', error.message)
+        logger.error('error connection to MongoDB:', error.message)
     })
 app.use(express.json())
+app.use(requestLogger)
 
-app.get('/api/blogs', (request, response) => {
-    Blog.find({}).then((blogs) => {
-        response.json(blogs)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog.save().then((result) => {
-        response.status(201).json(result)
-    })
-})
 
 const PORT =  process.env.PORT || 3001
 app.listen(PORT, () => {
