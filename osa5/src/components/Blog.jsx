f2bbfe97aa128ce87c5e6cd21503403ba8a9c998
mediatuottajavantaa <jyrgenhuhtala@gmail.com>
2026-07-29 12:54:38 +0300

    blogin lisätiedot voi avata tai sulkea nappulalla

--- osa5/src/components/Blog.jsx
+++ osa5/src/components/Blog.jsx
@@ -1,7 +1,30 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} by {blog.author}
-  </div>  
-)
+import {useState} from "react";
+
+const Blog = ({ blog }) => {
+    const [extended, setExtended, ] = useState(false);
+    const buttonText = extended ? "Hide" : "Show";
+
+    const toggleExtended = () => {
+        setExtended(!extended);
+    }
+
+        return (
+            <ul className="blogContainer">
+                <li>
+                    {blog.title} by {blog.author}
+                    <button onClick={toggleExtended}>{buttonText}</button>
+                </li>
+                {extended &&
+                    <>
+                        <li>{blog.url}</li>
+                        <li>likes: {blog.likes}
+                            <button>like</button>
+                        </li>
+                        <li>{blog.user.name}</li>
+                    </>
+                }
+            </ul>
+        )
+}
 
 export default Blog
