ee536c818f6eb7e70e09a31f083f047002ba2af7
mediatuottajavantaa <jyrgenhuhtala@gmail.com>
2026-08-25 12:04:34 +0300

    MUI stailaus Blog-komponentille

--- osa5/src/components/Blog.jsx
+++ osa5/src/components/Blog.jsx
@@ -1,3 +1,5 @@
+import { Button, ButtonGroup, Card } from '@mui/material'
+import { Link } from 'react-router-dom'
 
 const Blog = ({  blog, addALike, removeBlog, loggedUser }) => {
     const addedByUser = loggedUser?.username === blog?.user.username
@@ -5,19 +7,20 @@ const Blog = ({  blog, addALike, removeBlog, loggedUser }) => {
     if (!blog) return null
 
     return (
-        <ul className="blogContainer">
-            <li>
-                <h3>{blog.title} by {blog.author}</h3>
-            </li>
-            <>
-                <li>{blog.url}</li>
-                <li>likes: {blog.likes}
-                    {loggedUser && <button onClick={() => addALike(blog)}>like</button> }
+        <Card>
+            <ul className="blogContainer">
+                <li>
+                    <h2>{blog.title} by {blog.author}</h2>
                 </li>
-                <li>{blog.user.name}</li>
-                {addedByUser && <button onClick={() => removeBlog(blog)}>Remove</button> }
-            </>
-        </ul>
+                <li> <Link to={blog.url}>{blog.url}</Link></li>
+                <li>Added by {blog.user.name}</li>
+                <li> likes: {blog.likes}</li>
+            </ul>
+            <ButtonGroup size="medium">
+                {addedByUser && <Button color={'warning'} onClick={() => removeBlog(blog)}>Remove</Button> }
+                {loggedUser && <Button onClick={() => addALike(blog)}>like</Button> }
+            </ButtonGroup>
+        </Card>
     )
 }
 
